2a653fde45f9c26425228464267a09e895988f53
unknown <pmoniz7@hotmail.com>
2025-02-28 20:50:31 -0300

    Atualizei o arquivo main.py inseri prints

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def get_settings():
 @app.get("/")
 def read_root(settings: config.Settings = Depends(get_settings)):
     print("<<< % % % % %   the app_name configuration   % % % % % % >>>")
-    print(settings.app_name)
+    #print(settings.app_name)
     print(settings.APP_NAME)
     return "Hello World" 
 
