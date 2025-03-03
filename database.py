6be6eb336fcb3898230b3a23e88b6063b5c8a289
unknown <pmoniz7@hotmail.com>
2025-03-03 10:11:43 -0300

    retorno ao status do arquivo batabase.py

--- database.py
+++ database.py
@@ -14,17 +14,7 @@ host = os.environ['DATABASE_HOST']
 port = os.environ['DATABASE_PORT']
 db_name = os.environ['DATABASE_NAME']
 
-##SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
-
-
-
-##engine = create_engine(
-##    SQLALCHEMY_DATABASE_URL
-##)
-
-#XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-
-SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}:@{host}:{port}:/{db_name}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
 
 print("<<<<< SQLALCHEMY_DATABASE_URL >>>>> ", SQLALCHEMY_DATABASE_URL)
 
